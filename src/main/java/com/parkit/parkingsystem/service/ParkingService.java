dfe4dcf0ff9dbb7b5c9b6037cf865ecccee701fc
yaya <cherifi.yahia@yahoo.fr>
2020-04-14 04:50:16 +0200

    Fixing some code in the processExitingVehicle method: apply the 5% discount for leaving vehicles

--- src/main/java/com/parkit/parkingsystem/service/ParkingService.java
+++ src/main/java/com/parkit/parkingsystem/service/ParkingService.java
@@ -167,18 +167,25 @@ public class ParkingService {
             Ticket ticket = ticketDAO.getTicket(vehicleRegNumber);
             Date outTime = new Date();
             ticket.setOutTime(outTime);
-            fareCalculatorService.calculateFare(ticket);
+            int numberOfTickets = ticketDAO.getNumberOfTickets(
+                    ticket.getVehicleRegNumber());
+            if (numberOfTickets > 0) {
+                fareCalculatorService.calculateFareForRegularUsers(ticket);
+            } else {
+                fareCalculatorService.calculateFare(ticket);
+            }
+
             if (ticketDAO.updateTicket(ticket)) {
                 ParkingSpot parkingSpot = ticket.getParkingSpot();
                 parkingSpot.setAvailable(true);
                 parkingSpotDAO.updateParking(parkingSpot);
-                System.out.println("Please pay the parking fare:"
+                System.out.println("Please pay the parking fare: "
                         + ticket.getPrice());
-                System.out.println("Recorded out-time for vehicle number:"
+                System.out.println("Recorded out-time for vehicle number: "
                         + ticket.getVehicleRegNumber() + " is:" + outTime);
             } else {
-                System.out.println("Unable to update ticket information."
-                        + "Error occurred");
+                System.out.println("Unable to update "
+                        + "ticket information. Error occurred");
             }
         } catch (Exception e) {
             LOGGER.error("Unable to process exiting vehicle", e);
